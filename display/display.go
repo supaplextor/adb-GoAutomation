7a3b5175e179ed2eb502d03b4e16b76b6ab2294a
Scott Edwards <supaplextor@users.noreply.github.com>
2022-10-23 01:19:01 -0700

    Display go fixups (#15)
    * parsing two lines in adb shell wm size
    
    * parse adb shell wm size
    
    * typo in GOPATH

--- display/display.go
+++ display/display.go
@@ -22,21 +22,17 @@ func (disp Display) GetDisplaySize() (int, int, error) {
 	if err != nil {
 		return -1, -1, err
 	}
-	//fmt.Printf("wm size: %v", txt)
-	//fmt.Println()
+	var sizes []string = strings.Split(strings.Split(txt, "\r\n")[0], " ")
 
-	var sizes []string = strings.Split(strings.Split(txt, "size:")[1], "\r\n")
-	//fmt.Printf("[]string sizes == %v", sizes)
-	//fmt.Println()
 
 	if !strings.Contains(txt, "size:") {
 		return -1, -1, errors.New("not able to determine display size")
 	}
-	width, err := strconv.Atoi(strings.Split(strings.TrimSpace(sizes[0]), "x")[0])
+	width, err := strconv.Atoi(strings.Split(strings.TrimSpace(sizes[2]), "x")[0])
 	if err != nil {
 		return -1, -1, err
 	}
-	height, err := strconv.Atoi(strings.Split(strings.TrimSpace(sizes[0]), "x")[1])
+	height, err := strconv.Atoi(strings.Split(strings.TrimSpace(sizes[2]), "x")[1])
 	if err != nil {
 		return -1, -1, err
 	}
